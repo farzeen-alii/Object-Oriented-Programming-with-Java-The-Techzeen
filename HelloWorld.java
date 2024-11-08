7c2d31b2ab590b7179763377d7ea5b8f12156cb2
farzeen-alii <techzeen10@gmail.com>
2024-11-08 17:49:17 +0500

    Java Tutorial

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!!! From Farzeen");
+
+    }
+}
+
+//Single Comment
+//Pascal Convention: -> class
+//HelloWorld
+
+//Multi line comment
+/*
+Camel Convention: ->functions
+helloWorld
+*/
+
+
